cf3f4cf18beedf860d18525c3420ad6d34323019
Myrmex <dfusi@hotmail.com>
2024-03-21 13:58:48 +0100

    Updated packages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,52 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { AuthJwtInterceptor } from '@myrmidon/auth-jwt-login';
+import { EnvServiceProvider } from '@myrmidon/ng-tools';
+import { CadmusApiModule } from '@myrmidon/cadmus-api';
+
+import { provideMarkdown } from 'ngx-markdown';
+
+import { routes } from './app.routes';
+
+import { INDEX_LOOKUP_DEFINITIONS } from './index-lookup-definitions';
+import { ITEM_BROWSER_KEYS } from './item-browser-keys';
+import { PART_EDITOR_KEYS } from './part-editor-keys';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    provideMarkdown(),
+    EnvServiceProvider,
+    importProvidersFrom(CadmusApiModule),
+    // parts and fragments type IDs to editor group keys mappings
+    // https://github.com/nrwl/nx/issues/208#issuecomment-384102058
+    // inject like: @Inject('partEditorKeys') partEditorKeys: PartEditorKeys
+    {
+      provide: 'partEditorKeys',
+      useValue: PART_EDITOR_KEYS,
+    },
+    // index lookup definitions
+    {
+      provide: 'indexLookupDefinitions',
+      useValue: INDEX_LOOKUP_DEFINITIONS,
+    },
+    // item browsers IDs to editor keys mappings
+    // inject like: @Inject('itemBrowserKeys') itemBrowserKeys: { [key: string]: string }
+    {
+      provide: 'itemBrowserKeys',
+      useValue: ITEM_BROWSER_KEYS,
+    },
+    // HTTP interceptor
+    // https://medium.com/@ryanchenkie_40935/angular-authentication-using-the-http-client-and-http-interceptors-2f9d1540eb8
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthJwtInterceptor,
+      multi: true,
+    },
+  ],
 };
