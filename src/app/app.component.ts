7a84ade12a4fca1ab2b1539794a69ec900bdfdd4
Myrmex <dfusi@hotmail.com>
2024-03-21 15:17:34 +0100

    Wrapper modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,145 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { EnvService, EnvServiceProvider } from '@myrmidon/ng-tools';
+import {
+  User,
+  AuthJwtService,
+  GravatarService,
+} from '@myrmidon/auth-jwt-login';
+
+// Cadmus bricks
+import { CadmusRefsAssertedIdsModule } from '@myrmidon/cadmus-refs-asserted-ids';
+import { CadmusRefsDocReferencesModule } from '@myrmidon/cadmus-refs-doc-references';
+import { CadmusRefsHistoricalDateModule } from '@myrmidon/cadmus-refs-historical-date';
+// Cadmus
+import {
+  CadmusCoreModule,
+  Thesaurus,
+  ThesaurusEntry,
+} from '@myrmidon/cadmus-core';
+import { CadmusGraphPgModule } from '@myrmidon/cadmus-graph-pg';
+import { CadmusGraphUiModule } from '@myrmidon/cadmus-graph-ui';
+import { CadmusProfileCoreModule } from '@myrmidon/cadmus-profile-core';
+import { AppRepository, CadmusStateModule } from '@myrmidon/cadmus-state';
+import { CadmusUiModule } from '@myrmidon/cadmus-ui';
+import { CadmusUiPgModule } from '@myrmidon/cadmus-ui-pg';
+import { CadmusItemEditorModule } from '@myrmidon/cadmus-item-editor';
+import { CadmusItemListModule } from '@myrmidon/cadmus-item-list';
+import { CadmusItemSearchModule } from '@myrmidon/cadmus-item-search';
+import { CadmusThesaurusEditorModule } from '@myrmidon/cadmus-thesaurus-editor';
+import { CadmusThesaurusListModule } from '@myrmidon/cadmus-thesaurus-list';
+import { CadmusThesaurusUiModule } from '@myrmidon/cadmus-thesaurus-ui';
+import { CadmusUiFlagsPickerModule } from '@myrmidon/cadmus-ui-flags-picker';
+
+import { MonacoWrapperModule } from './monaco-wrapper.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterModule,
+    RouterOutlet,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MonacoWrapperModule,
+    // Cadmus
+    CadmusRefsDocReferencesModule,
+    CadmusRefsHistoricalDateModule,
+    CadmusRefsAssertedIdsModule,
+    CadmusCoreModule,
+    CadmusProfileCoreModule,
+    CadmusStateModule,
+    CadmusUiModule,
+    CadmusUiPgModule,
+    CadmusGraphPgModule,
+    CadmusGraphUiModule,
+    CadmusItemEditorModule,
+    CadmusItemListModule,
+    CadmusItemSearchModule,
+    CadmusThesaurusEditorModule,
+    CadmusThesaurusListModule,
+    CadmusThesaurusUiModule,
+    CadmusUiFlagsPickerModule,
+  ],
+  providers: [EnvServiceProvider],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'cadmus-febo-app';
+export class AppComponent implements OnInit, OnDestroy {
+  private _subs: Subscription[];
+
+  public user?: User;
+  public logged?: boolean;
+  public itemBrowsers?: ThesaurusEntry[];
+  public version: string;
+
+  constructor(
+    @Inject('itemBrowserKeys')
+    private _itemBrowserKeys: { [key: string]: string },
+    private _authService: AuthJwtService,
+    private _gravatarService: GravatarService,
+    private _appRepository: AppRepository,
+    private _router: Router,
+    env: EnvService
+  ) {
+    this.version = env.get('version') || '';
+    this._subs = [];
+  }
+
+  ngOnInit(): void {
+    this.user = this._authService.currentUserValue || undefined;
+    this.logged = this.user !== null;
+
+    this._subs.push(
+      this._authService.currentUser$.subscribe((user: User | null) => {
+        this.logged = this._authService.isAuthenticated(true);
+        this.user = user || undefined;
+        if (user) {
+          this._appRepository.load();
+        }
+      })
+    );
+
+    this._subs.push(
+      this._appRepository.itemBrowserThesaurus$.subscribe(
+        (thesaurus: Thesaurus | undefined) => {
+          this.itemBrowsers = thesaurus ? thesaurus.entries : undefined;
+        }
+      )
+    );
+  }
+
+  ngOnDestroy(): void {
+    this._subs.forEach((sub) => {
+      sub.unsubscribe();
+    });
+  }
+
+  public getItemBrowserRoute(id: string): string {
+    return this._itemBrowserKeys[id] || id;
+  }
+
+  public getGravatarUrl(email: string, size = 80): string | null {
+    return this._gravatarService.buildGravatarUrl(email, size);
+  }
+
+  public logout(): void {
+    if (!this.logged) {
+      return;
+    }
+    this._authService.logout().subscribe((_) => {
+      this._router.navigate(['/home']);
+    });
+  }
 }
